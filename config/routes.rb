93dd0a82d46c37623b98b967461bb8b7ac39ec0e
Your Name <you@example.com>
2022-02-01 11:38:23 +1100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
-Rails.application.routes.draw do
-  get 'user/name:string'
-  get 'user/email:text'
-  get 'user/password_digest:text'
-  root to: "pages#home"
-  get "/login" => "session#new"
-  post "/login" => "session#create" #form submitts here
-  delete "/login" => "session#destroy"
+# Rails.application.routes.draw do
+#   get "user/name:string"
+#   get "user/email:text"
+#   get "user/password_digest:text"
+#   root to: "pages#home"
+#   get "/login" => "session#new"
+#   post "/login" => "session#create" #form submitts here
+#   delete "/login" => "session#destroy"
 
-  resources :users
-  resources :posts
-  resources :comments
-  # get "/users/new" => "users#new", as: "new_users"
-  # get "/users" => "users#index"
-  # post "/users" => "users#create"
-end
+# resources :users
+# resources :posts
+# resources :comments
+# get "/users/new" => "users#new", as: "new_users"
+# get "/users" => "users#index"
+# post "/users" => "users#create"
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
-# created data base for user and posts
+# # created data base for user and posts
 
-print "Creating posts..."
-Post.destroy_all
-u1 = User.create! post: name:
-u2 = User.create! post: name:
-u3 = User.cretae! post: name:
-u4 = User.create! post: name:
+# print "Creating posts..."
+# Post.destroy_all
+# u1 = User.create! post: name:
+# u2 = User.create! post: name:
+# u3 = User.cretae! post: name:
+# u4 = User.create! post: name:
 
-puts "created #{Post.count} 
-users."
+# puts "created #{Post.count}
+# users."
 
-print "Creating comments..."
-Comment.destroy_all
-u1 = User.create! comment: name:
-u2 = User.create! comment:
-u3 = User.cretae! comment:
-u4 = User.create! comment:
+# print "Creating comments..."
+# Comment.destroy_all
+# u1 = User.create! comment: name:
+# u2 = User.create! comment:
+# u3 = User.cretae! comment:
+# u4 = User.create! comment:
 
-puts "created #{Comment.count} 
-users."
+# puts "created #{Comment.count}
+# users."
 
-print "Creating users..."
-User.destroy_all
-u1 = User.create! email: "Suraj@ga.co", name: "suraj", password: "chicken"
-u2 = User.create! email: "Dan@ga.co", name: "dan", password: "chicken"
-u3 = User.create! email: "Steven@ga.co", name: "steven", password: "chicken"
-u4 = User.create! email: "Franki@ga.co", name: "franki", password: "chicken"
+# print "Creating users..."
+# User.destroy_all
+# u1 = User.create! email: "Suraj@ga.co", name: "suraj", password: "chicken"
+# u2 = User.create! email: "Dan@ga.co", name: "dan", password: "chicken"
+# u3 = User.create! email: "Steven@ga.co", name: "steven", password: "chicken"
+# u4 = User.create! email: "Franki@ga.co", name: "franki", password: "chicken"
 
-puts "created #{User.count} users."
+# puts "created #{User.count} users."
